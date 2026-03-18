6d61675908455eb3a2cdcb80c664c5a445ba3c57
Timothy Whittaker <tdw419@gmail.com>
2026-03-18 14:04:33 -0500

    feat(renderer): enable all exports in barrel file

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,8 +1,7 @@
 // src/renderer/index.ts
 
 export * from './types';
-// TODO: Uncomment when components are implemented
-// export * from './components/AsciiView';
-// export * from './components/LabelButton';
-// export * from './components/StateView';
-// export * from './hooks/useAsciiState';
+export * from './components/AsciiView';
+export * from './components/LabelButton';
+export * from './components/StateView';
+export * from './hooks/useAsciiState';
